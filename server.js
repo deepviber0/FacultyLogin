4c307348b6a3c9de55dea897ede7ce566989b5f2
deepviber0 <deepcoders0@gmail.com>
2025-01-11 23:46:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,9 @@ app.post('/api/login', async (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
